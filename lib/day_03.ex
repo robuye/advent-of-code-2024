4ced42f358747496429ea5978cf42e93ee1bbc47
robuye <rulejczyk@gmail.com>
2024-12-03 10:05:36 +0100

    feat: day 3, part 2

--- lib/day_03.ex
+++ lib/day_03.ex
@@ -2,24 +2,52 @@ defmodule Day03 do
   def find_the_answer_p1() do
     "data/day_03.txt"
     |> read_the_input()
-    |> parse()
     |> play_the_game_p1()
   end
 
+  def find_the_answer_p2() do
+    "data/day_03.txt"
+    |> read_the_input()
+    |> play_the_game_p2()
+  end
+
   def play_the_game_p1(input) do
-    input
-    |> Enum.map(fn [a, b] -> a * b end)
+    ~r/mul\(\d{1,3},\d{1,3}\)/
+    |> Regex.scan(input)
+    |> List.flatten()
+    |> Enum.map(&parse/1)
     |> Enum.sum()
   end
 
-  def parse(input) do
-    Regex.scan(~r/mul\(\d{1,3},\d{1,3}\)/, input)
+  def play_the_game_p2(input) do
+    ~r/(mul\(\d{1,3},\d{1,3}\)|do\(\)|don't\(\))/
+    |> Regex.scan(input, capture: :all_but_first)
     |> List.flatten()
-    |> Enum.map(&Regex.scan(~r/(\d+),(\d+)/, &1, capture: :all_but_first))
-    |> List.flatten()
-    |> Enum.map(&String.to_integer/1)
-    |> Enum.chunk_every(2)
+    |> Enum.map(&parse/1)
+    |> Enum.reduce(%{state: :accept, items: []}, fn
+      n, acc when is_number(n) ->
+        if acc.state == :accept do
+          Map.put(acc, :items, [n | acc.items])
+        else
+          acc
+        end
+
+      action, acc ->
+        %{acc | state: action}
+    end)
+    |> then(&Enum.sum(&1.items))
   end
+
+  def parse("mul" <> _ = val) do
+    [a, b] =
+      Regex.run(~r/(\d+),(\d+)/, val, capture: :all_but_first)
+      |> Enum.map(&String.to_integer/1)
+
+    a * b
+  end
+
+  def parse("do()"), do: :accept
+  def parse("don't()"), do: :reject
 
   def read_the_input(path) do
     path
