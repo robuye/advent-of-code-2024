bc1a72c9ba6d0ca0a14c8f9551767a4a4004c25a
robuye <rulejczyk@gmail.com>
2024-12-05 11:47:42 +0100

    feat: day 5, part 2

--- lib/day_05.ex
+++ lib/day_05.ex
@@ -7,12 +7,57 @@ defmodule Day05 do
     |> play_the_game_p1()
   end
 
+  def find_the_answer_p2() do
+    "data/day_05.txt"
+    |> read_the_input()
+    |> parse()
+    |> split_pages()
+    |> play_the_game_p2()
+  end
+
   def play_the_game_p1(state) do
     state.valid_pages
     |> Enum.map(fn pages ->
       Enum.at(pages, trunc(length(pages) / 2))
     end)
     |> Enum.sum()
+  end
+
+  def play_the_game_p2(state) do
+    state.invalid_pages
+    |> Enum.map(fn pages ->
+      matching_rules =
+        state.rules
+        |> Enum.filter(fn {a, b} ->
+          Enum.member?(pages, a) and Enum.member?(pages, b)
+        end)
+
+      fix_pages_ordering(pages, matching_rules)
+    end)
+    |> Enum.map(fn pages ->
+      Enum.at(pages, trunc(length(pages) / 2))
+    end)
+    |> Enum.sum()
+  end
+
+  def fix_pages_ordering(pages, rules) do
+    rules
+    |> Enum.reduce(pages, fn {a, b}, acc ->
+      position_a = Enum.find_index(acc, &(&1 == a))
+      position_b = Enum.find_index(acc, &(&1 == b))
+
+      if position_a < position_b do
+        acc
+      else
+        value_at_a = Enum.at(acc, position_a)
+        value_at_b = Enum.at(acc, position_b)
+
+        acc
+        |> List.replace_at(position_a, value_at_b)
+        |> List.replace_at(position_b, value_at_a)
+        |> fix_pages_ordering(rules)
+      end
+    end)
   end
 
   def split_pages(state) do
