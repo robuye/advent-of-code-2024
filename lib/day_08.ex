b85aeeea9773b400e139527a149052d7eeae1d85
robuye <rulejczyk@gmail.com>
2024-12-08 20:36:25 +0100

    feat: day 8, part 2

--- lib/day_08.ex
+++ lib/day_08.ex
@@ -6,6 +6,13 @@ defmodule Day08 do
     |> play_the_game_p1()
   end
 
+  def find_the_answer_p2() do
+    "data/day_08.txt"
+    |> read_the_input()
+    |> parse()
+    |> play_the_game_p2()
+  end
+
   def play_the_game_p1(input) do
     antennas =
       input
@@ -31,18 +38,51 @@ defmodule Day08 do
       antennas
       |> Enum.reduce(initial_state, fn antenna, state ->
         state
-        |> place_antinodes(antenna)
+        |> place_antinodes_p1(antenna)
         |> place_antenna(antenna)
       end)
 
     length(result.antinodes)
   end
 
+  def play_the_game_p2(input) do
+    antennas =
+      input
+      |> Enum.with_index()
+      |> Enum.flat_map(fn {row, y} ->
+        row
+        |> Enum.with_index()
+        |> Enum.map(fn
+          {".", _x} -> nil
+          {antenna, x} -> %{x: x, y: y, freq: antenna}
+        end)
+        |> Enum.filter(& &1)
+      end)
+
+    initial_state = %{
+      max_x: length(hd(input)) - 1,
+      max_y: length(input) - 1,
+      antennas: [],
+      antinodes: []
+    }
+
+    result =
+      antennas
+      |> Enum.reduce(initial_state, fn antenna, state ->
+        state
+        |> place_antinodes_p2(antenna)
+        |> place_antenna(antenna)
+      end)
+
+    length(result.antinodes)
+    # result
+  end
+
   defp place_antenna(state, antenna) do
     %{state | antennas: [antenna | state.antennas]}
   end
 
-  defp place_antinodes(state, antenna) do
+  defp place_antinodes_p1(state, antenna) do
     new_antinodes =
       state.antennas
       |> Enum.flat_map(fn
@@ -91,6 +131,64 @@ defmodule Day08 do
     all_antinodes =
       (state.antinodes ++ new_antinodes)
       |> Enum.uniq_by(&{&1.x, &1.y})
+
+    %{state | antinodes: all_antinodes}
+  end
+
+  defp place_antinodes_p2(state, antenna) do
+    new_antinodes =
+      state.antennas
+      |> Enum.flat_map(fn
+        other_antenna when antenna.freq == other_antenna.freq ->
+          distance_x = abs(antenna.x - other_antenna.x)
+          distance_y = abs(antenna.y - other_antenna.y)
+
+          direction_x = if(other_antenna.x > antenna.x, do: :right, else: :left)
+          direction_y = if(other_antenna.y > antenna.y, do: :down, else: :up)
+
+          antinodes =
+            Stream.iterate({antenna.x, antenna.y}, fn {prev_x, prev_y} ->
+              next_antinode_x =
+                if(direction_x == :right, do: prev_x - distance_x, else: prev_x + distance_x)
+
+              next_antinode_y =
+                if(direction_y == :down, do: prev_y - distance_y, else: prev_y + distance_y)
+
+              {next_antinode_x, next_antinode_y}
+            end)
+            |> Stream.take_while(fn {x, y} ->
+              x >= 0 and x <= state.max_x and y >= 0 and y <= state.max_y
+            end)
+            |> Enum.to_list()
+
+          # change direction
+          direction_x = if(other_antenna.x < antenna.x, do: :right, else: :left)
+          direction_y = if(other_antenna.y < antenna.y, do: :down, else: :up)
+
+          opposite_antinodes =
+            Stream.iterate({other_antenna.x, other_antenna.y}, fn {prev_x, prev_y} ->
+              next_antinode_x =
+                if(direction_x == :right, do: prev_x - distance_x, else: prev_x + distance_x)
+
+              next_antinode_y =
+                if(direction_y == :down, do: prev_y - distance_y, else: prev_y + distance_y)
+
+              {next_antinode_x, next_antinode_y}
+            end)
+            |> Stream.take_while(fn {x, y} ->
+              x >= 0 and x <= state.max_x and y >= 0 and y <= state.max_y
+            end)
+            |> Enum.to_list()
+
+          antinodes ++ opposite_antinodes
+
+        _other_antenna ->
+          []
+      end)
+
+    all_antinodes =
+      (state.antinodes ++ new_antinodes)
+      |> Enum.uniq()
 
     %{state | antinodes: all_antinodes}
   end
